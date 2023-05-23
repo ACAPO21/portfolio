584fb12ec034dc84402dfa8433d0256db3230280
ACAPO1 <arthur.capo@ynov.com>
2023-05-23 23:19:00 +0200

    corrige bug

--- js/parallax.js
+++ js/parallax.js
@@ -1,4 +1,19 @@
-window.addEventListener('scroll', function() {
-var parallax = document.querySelector('section img');
-parallax.style.transform = 'translate3d(0, ' + window.pageYOffset * 1.1 + 'px, 0)';
-});
+function parallaxScroll() {
+    var scrollPosition = window.pageYOffset;
+  
+    var images = document.querySelectorAll('section img');
+    images.forEach(function(image) {
+      var section = image.closest('section');
+      var sectionTop = section.offsetTop;
+      var sectionHeight = section.offsetHeight;
+      var sectionBottom = sectionTop + sectionHeight;
+  
+      if (scrollPosition + window.innerHeight > sectionTop && scrollPosition < sectionBottom) {
+        var parallaxValue = (scrollPosition - sectionTop) * 0.5;
+        image.style.transform = 'translate3d(0, ' + parallaxValue + 'px, 0)';
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', parallaxScroll);
+  
